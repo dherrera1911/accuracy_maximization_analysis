54650a0ab02d1468f5a70300962426649f42ac16
dherresp-adm <dherrera1911@gmail.com>
2024-12-23 12:11:30 -0300

    Add constraint to fix filters in place

--- src/amatorch/constraints.py
+++ src/amatorch/constraints.py
@@ -3,6 +3,7 @@ Constraints to keep the filters on a certain set (e.g. sphere), or to fix
 some filter values during training.
 """
 
+import torch
 import torch.nn as nn
 
 import amatorch.normalization as normalization
@@ -54,3 +55,91 @@ class Sphere(nn.Module):
             Returns the input tensor `S`.
         """
         return S
+
+
+class FixedFilters(nn.Module):
+    """Fix some of the filters to prevent updating with gradient descent."""
+
+    def __init__(self, n_row_fixed):
+        """
+        Initialize the FixedFilters class.
+
+        Parameters
+        ----------
+        value : torch.Tensor
+            Value to fix the filters to.
+        """
+        super().__init__()
+        self.n_row_fixed = n_row_fixed
+
+    def forward(self, X):
+        """
+        Concatenate the fixed tensor with the input tensor.
+
+        Parameters
+        ----------
+        X : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Fixed value.
+        """
+        fixed_tensor = X[: self.n_row_fixed].detach()
+        return torch.cat([fixed_tensor, X[self.n_row_fixed :]], dim=0)
+
+    def right_inverse(self, X):
+        """
+        Return only the rows after the fixed tensor.
+
+        Parameters
+        ----------
+        X : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Returns the non-fixed part of the tensor.
+        """
+        return X
+
+
+# Define the unconstrained constraint
+class Identity(nn.Module):
+    """Leaves the input tensor unconstrained. Used for consistency."""
+
+    def forward(self, X):
+        """
+        Return the input tensor as is.
+
+        Parameters
+        ----------
+        X : torch.Tensor
+            Input tensor (n_filters, n_dim).
+
+        Returns
+        -------
+        torch.Tensor
+            Normalized tensor lying on the sphere with shape
+            (n_filters, n_dim).
+        """
+        return X
+
+    def right_inverse(self, S):
+        """
+        Identity function.
+
+        Parameters
+        ----------
+        S : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Returns the input tensor.
+        """
+        return S
+
